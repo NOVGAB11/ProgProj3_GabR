a2e888489941650f180458a16a12d356c878acc6
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-06 13:50:19 -0600

    Added the Button "Talk to me"

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,13 @@
       <h2>Click on the button for a message from the Cosmos...</h2>
     </section>
 
+    <section class="UserSection">
+
+      <button @click="showFortune" id="UserButton" type="button">Talk to me</button>
+      <p v-if="fortuneMessage" id ="UsersFortune">{{ fortuneMessage}}</p>
+      
+    </section>
+
 
 
   
