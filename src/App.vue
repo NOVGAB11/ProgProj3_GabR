d634c8073d514f15094b7408727e89f906efe989
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-06 13:03:56 -0600

    Deleted previous template... created a new section called greeting

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,18 @@
 </script>
 
 <template>
-  <main class="app">
-
+  <main class="app"> 
+   
     <section class="greeting">
-
-
+      <h1 class="main-title">Hi! I am the Medium between you and the Cosmos!</h1>
+      <h2>Click on the button for a message from the Cosmos...</h2>
     </section>
 
 
 
-    <section class="userSection">
+  
 
 
-    </section>
-
-
-    <section class="message">
-
-      
-    </section>
 
 
 
