f2b9eaa7724fa44339a5528f0e8add9670b6c320
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-02-06 14:15:05 -0600

    space

--- src/App.vue
+++ src/App.vue
@@ -38,8 +38,6 @@ const fortunes = [
       <source src="src\assets\perfect-beauty-191271.mp3" type="audio/mp3"/>
     </audio>
 
-
-
     <section class="greeting">
       <h1 class="main-title">Hi! I am the Medium between you and the Cosmos!</h1>
       <h2>Click on the button for a message from the Cosmos...</h2>
